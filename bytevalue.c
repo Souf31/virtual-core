0cd5a973c6525126ca98ac3ae79fb67c6831c7c2
Souf31 <saifisophiane@gmail.com>
2023-02-22 16:53:52 +0100

    added translation of bits 4 by 4

--- bytevalue.c
+++ bytevalue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
+void decode() {
     int byte[31];
     unsigned char a=0, b=53, c=129, d=0;
     unsigned long combined = (a << 24) | (b << 16) | (c << 8) | d;
@@ -9,4 +9,36 @@ void main() {
     byte[i]=(combined & (1 << i)) ? 1:0;
     printf("%d",byte[i]);
     printf("|");}
+    printf("\n");
+}
+
+void translate() {
+    int bits[11];
+    bits[0]=0;
+    bits[1]=1;
+    bits[2]=1;
+    bits[3]=0;
+    
+    bits[4]=0;
+    bits[5]=0;
+    bits[6]=1;
+    bits[7]=0;
+
+    bits[8]=1;
+    bits[9]=1;
+    bits[10]=1;
+    bits[11]=1;
+
+    for (int i=0; i<11; i=i+4){
+
+    unsigned long combined = (bits[i]<<3) | (bits[i+1]<<2) | (bits[i+2]<<1) | (bits[i+3]);
+
+    printf("%lu", combined); 
+    printf("\n");
+    
+    }
+}
+
+int main(){
+    return 0;
 }
--- core.c
+++ core.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_binary(unsigned char n) {
-    int i;
-    for (i = 7; i >= 0; i--) {
-        printf("%d", (n & (1 << i)) ? 1 : 0);
+void decode(unsigned char* bytes, int bytesLength){
+    int bits[31];
+    for (int i=0; i<bytesLength; i=i+4){
+    unsigned char a=bytes[i], b=bytes[i+1], c=bytes[i+2], d=bytes[i+3];
+    unsigned long combined = (a << 24) | (b << 16) | (c << 8) | d;
+    for (int i=31; i>=0; i--){
+    bits[i]=(combined & (1 << i)) ? 1:0;
+    printf("%d",bits[i]);
+    printf("|");}
+    printf("\n");
     }
 }
 
@@ -37,11 +43,13 @@ int main()
     fread(data, 1, size, fp);
 
     // Use the data as needed
-    for (i = 0; i < size; i++) {
-        printf("Data[%d] = ", i);
-        print_binary(data[i]);
-        printf("\n");
-    }
+    // for (i = 0; i < size; i++) {
+    //     printf("Data[%d] = ", i);
+    //     printf("%d", data[i]);
+    //     printf("\n");
+    // }
+
+    decode(data, size);
 
     // Clean up
     fclose(fp);
